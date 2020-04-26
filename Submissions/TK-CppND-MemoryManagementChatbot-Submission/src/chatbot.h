c2a1a2c4db49e965c7635d6138be2550f9cff6b7
tkayadelen <kayadelen.tolga@gmail.com>
2020-04-26 16:57:12 +0100

    Adding CPP-ND Memory Managemement Project

---
+++ Submissions/TK-CppND-MemoryManagementChatbot-Submission/src/chatbot.h
@@ -0,0 +1,59 @@
+#ifndef CHATBOT_H_
+#define CHATBOT_H_
+
+#include <wx/bitmap.h>
+#include <string>
+#include <memory>
+
+class GraphNode; // forward declaration
+class ChatLogic; // forward declaration
+
+class ChatBot
+{
+private:
+    // data handles (owned)
+    wxBitmap *_image; // avatar image
+
+    // data handles (not owned)
+    GraphNode *_currentNode;
+    GraphNode *_rootNode;
+    ChatLogic *_chatLogic;
+
+    // proprietary functions
+    int ComputeLevenshteinDistance(std::string s1, std::string s2);
+
+public:
+    // constructors / destructors
+    ChatBot();                     // constructor WITHOUT memory allocation
+    ChatBot(std::string filename); // constructor WITH memory allocation
+    ~ChatBot();
+
+    //// STUDENT CODE
+    ////
+
+    // Task 2: Make ChatBot compliant with rule of five.
+    // Copy Constructor
+    ChatBot(const ChatBot &source);
+
+    // Copy assignment operator
+    ChatBot &operator=(const ChatBot &source);
+
+    // Move constructor
+    ChatBot(ChatBot &&source);
+
+    // Move Assignment operator
+    ChatBot& operator=(ChatBot &&source);
+    ////
+    //// EOF STUDENT CODE
+
+    // getters / setters
+    void SetCurrentNode(GraphNode *node);
+    void SetRootNode(GraphNode *rootNode) { _rootNode = rootNode; }
+    void SetChatLogicHandle(ChatLogic *chatLogic) { _chatLogic = chatLogic; }
+    wxBitmap *GetImageHandle() { return _image; }
+
+    // communication
+    void ReceiveMessageFromUser(std::string message);
+};
+
+#endif /* CHATBOT_H_ */
